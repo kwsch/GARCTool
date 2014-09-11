76a9b0a5585d4f81fe5d7848310a217754668c0d
Kurt <kaphotics@gmail.com>
2014-09-10 20:04:03 -0700

    Initial Commit

---
+++ GARCTool/Interface.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace GARCTool
+{
+    partial class Interface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.B_OpenFolder = new System.Windows.Forms.Button();
+            this.B_Process = new System.Windows.Forms.Button();
+            this.TB_Path = new System.Windows.Forms.TextBox();
+            this.B_OpenFile = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // B_OpenFolder
+            // 
+            this.B_OpenFolder.Location = new System.Drawing.Point(12, 12);
+            this.B_OpenFolder.Name = "B_OpenFolder";
+            this.B_OpenFolder.Size = new System.Drawing.Size(75, 23);
+            this.B_OpenFolder.TabIndex = 0;
+            this.B_OpenFolder.Text = "Open Folder";
+            this.B_OpenFolder.UseVisualStyleBackColor = true;
+            this.B_OpenFolder.Click += new System.EventHandler(this.B_Open_Click);
+            // 
+            // B_Process
+            // 
+            this.B_Process.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.B_Process.Location = new System.Drawing.Point(196, 12);
+            this.B_Process.Name = "B_Process";
+            this.B_Process.Size = new System.Drawing.Size(75, 23);
+            this.B_Process.TabIndex = 1;
+            this.B_Process.Text = "Process";
+            this.B_Process.UseVisualStyleBackColor = true;
+            this.B_Process.Click += new System.EventHandler(this.B_Process_Click);
+            // 
+            // TB_Path
+            // 
+            this.TB_Path.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TB_Path.Location = new System.Drawing.Point(12, 41);
+            this.TB_Path.Name = "TB_Path";
+            this.TB_Path.Size = new System.Drawing.Size(259, 20);
+            this.TB_Path.TabIndex = 2;
+            // 
+            // B_OpenFile
+            // 
+            this.B_OpenFile.Location = new System.Drawing.Point(105, 12);
+            this.B_OpenFile.Name = "B_OpenFile";
+            this.B_OpenFile.Size = new System.Drawing.Size(75, 23);
+            this.B_OpenFile.TabIndex = 3;
+            this.B_OpenFile.Text = "Open File";
+            this.B_OpenFile.UseVisualStyleBackColor = true;
+            this.B_OpenFile.Click += new System.EventHandler(this.B_OpenFile_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(12, 67);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(259, 13);
+            this.progressBar.TabIndex = 4;
+            // 
+            // Interface
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 92);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.B_OpenFile);
+            this.Controls.Add(this.TB_Path);
+            this.Controls.Add(this.B_Process);
+            this.Controls.Add(this.B_OpenFolder);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 130);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(300, 130);
+            this.Name = "Interface";
+            this.Text = "GARCTool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button B_OpenFolder;
+        private System.Windows.Forms.Button B_Process;
+        private System.Windows.Forms.TextBox TB_Path;
+        private System.Windows.Forms.Button B_OpenFile;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ GARCTool/Interface.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GARCTool
+{
+    public partial class Interface : Form
+    {
+        public Interface()
+        {
+            InitializeComponent();
+            this.DragEnter += new DragEventHandler(Form_DragEnter);
+            this.DragDrop += new DragEventHandler(Form_DragDrop);
+        }
+        private void Form_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+        private void Form_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            string path = files[0]; // open first D&D
+            TB_Path.Text = path;
+        }
+
+        private void B_Open_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+                TB_Path.Text = fbd.SelectedPath;
+        }
+        private void B_OpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+                TB_Path.Text = ofd.FileName;
+        }
+        private void B_Process_Click(object sender, EventArgs e)
+        {
+            if (TB_Path.Text.Length == 0)
+            { MessageBox.Show("No Path Loaded"); return; }
+            GARCTool.garcOmni(TB_Path.Text, progressBar);
+        }
+    }
+}
